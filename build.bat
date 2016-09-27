ebc5cdd96ab42af095175661174c87101d5f29b1
Jolty <juplagon@gmail.com>
2016-09-27 19:44:51 +0200

    V0.1 alfa
    Primera compilacion. Arreglado build.bat a la version 1.1 estable.
    Primera excepcion controlada de la aplicación.

--- build.bat
+++ build.bat
@@ -1,17 +1,21 @@
 @echo off
-
+cls
 echo Script de compilacion de UmbJolt3D por Jolty95
 echo =======================================
-echo Versión: 1.0
-echo Proyecto: UmbJolt3D por Jolty95 & Esky73
+echo Version: 1.1
+echo Proyecto: UmbJolt3D por Jolty95 ^& Esky73
 echo =======================================
 if "%1" == "clean" goto clean
 echo Borrando el directorio romfs....
-del Builder\romfs\index.lua
+IF EXIST Builder\romfs rmdir /S /Q Builder\romfs
 echo Copiando el script actualizado al directorio romfs...
 ::Copia el nuevo índice a romsfs
+echo Creando directorio...
 mkdir Builder\romfs
+echo Creado.
+echo Copiando index.lua...
 copy index.lua Builder\romfs\index.lua
+echo Copiado.
 echo Compilando romfs en el directorio...
 cd Builder
 tools\3dstool -cvtf romfs romfs.bin --romfs-dir romfs
@@ -23,52 +27,58 @@ if %1 == 3dsx-b goto 3dsx
 goto %1
 
 :all
+echo =============================
 echo Compilando en formato cia y 3dsx...
 call :cia-b
 call :3dsx-b
-echo Compilación terminada.
+echo Compilacion terminada.
 cd ..
 exit /b
 
 :3dsx
+echo =============================
 echo Compilando en formato 3dsx...
 call :3dsx-b
-echo Compilación terminada.
+echo Compilacion terminada.
 cd ..
 exit /b
 :cia
+echo =============================
 echo Compilando en formato cia...
 call :cia-b
-echo Compilación terminada.
+echo Compilacion terminada.
 cd ..
 exit /b
 
 :3dsx-b
-echo Creando información SMDH...
-tools\bannertool makesmdh -s "UmbJolt3D" -l "La creación de Jolty y Esky" -p "Jolty95 & Esky73" -i UmbJolt3DLogo.png -o UmbJolt3D.smdh
+echo Creando informacion SMDH...
+tools\bannertool makesmdh -s "UmbJolt3D" -l "La creacion de Jolty y Esky" -p "Jolty95 & Esky73" -i UmbJolt3DLogo.png -o UmbJolt3D.smdh
+echo Informacion SMDH creada.
 echo Creando archivo 3DSX...
-tools\3dsxtool bin/lpp-3ds.elf ../UmbJolt3D.3dsx --romfs=romfs.bin --smdh=UmbJolt3D.smdh
+tools\3dsxtool bin/lpp-3ds.elf ../UmbJolt3D.3dsx --romfs=romfs.bin --smdh=UmbJolt3D.smdh 
+echo Archivo 3DSX creado.
 goto :EOF
 
 :cia-b
 echo Creando banner desde los archivos...
 tools\bannertool makebanner -i banner.png -a sonido.wav -o banner.bin
+echo Banner creado.
 echo Creando icono desde archivo...
-tools\bannertool makesmdh -s "UmbJolt3D" -l "La creación de Jolty y Esky" -p "Jolty95 & Esky73" -i UmbJolt3DLogo.png -o icono.bin
+tools\bannertool makesmdh -s "UmbJolt3D" -l "La creacion de Jolty y Esky" -p "Jolty95 & Esky73" -i UmbJolt3DLogo.png -o icono.bin
+echo Icono creado.
 echo Creando archivo CIA ...
-::RSF mas adelante
-::tools\makerom -f cia -o ../UmbJolt3D.cia -elf bin/lpp-3ds.elf -rsf UmbJolt3D.rsf -icon icon.bin -banner banner.bin -exefslogo -target t -romfs romfs.bin
-tools\makerom -f cia -o ../UmbJolt3D.cia -elf bin/lpp-3ds.elf -icon icon.bin -banner banner.bin -exefslogo -target t -romfs romfs.bin
+tools\makerom -f cia -o ../UmbJolt3D.cia -elf bin/lpp-3ds.elf -rsf UmbJolt3D.rsf -icon icono.bin -banner banner.bin -exefslogo -target t -romfs romfs.bin
+echo Archivo CIA creado.
 goto :EOF
 
 :clean
 echo Limpiando...
 cd Builder
-del ..\*.3dsx
-del ..\*.cia
-del romfs\*.lua
-del *.bin
-del *.smdh
+IF EXIST ..\*.3dsx del /F /Q ..\*.3dsx
+IF EXIST ..\*.cia del /F /Q ..\*.cia
+IF EXIST romfs rmdir /S /Q romfs
+IF EXIST *.bin del /F /Q *.bin
+IF EXIST *.smdh del /F /Q *.smdh
 cd ..
-echo Operación terminada.
+echo Operacion terminada.
 exit /b
--- index.lua
+++ index.lua
@@ -1,2 +1,90 @@
 --En desarrollo. Listo para programar
 --doc: http://rinnegatamante.it/lpp-3ds_doc.html
+
+
+--Variables
+local pad = Controls.read()
+local antpad = pad
+local usuario
+local cumpledia, cumplemes
+local firm1, firm2, firm3
+local kernel1, kernel2, kernel3
+local region
+local bateria
+local h, m, s
+local d, mes, ano
+
+local fecha
+local hora
+local cumple
+local firmware
+local kernel
+
+
+
+--Colores
+local colores =
+{	
+	rojo = Color.new(255,0,0),
+	verde = Color.new(55,255,0),
+	amarillo = Color.new(255,205,66),
+	blanco = Color.new(255,255,255),
+	--un azul distinto al "estandar"
+	azul = Color.new(100,149,237)	
+}
+
+--Main
+function main()
+    Screen.refresh()
+    Screen.clear(TOP_SCREEN)
+	Screen.clear(BOTTOM_SCREEN)
+	Screen.fillRect(0,0, 1000, 1000, colores.azul, BOTTOM_SCREEN)
+	getVariables()
+	hola = "Hola, "
+	hola += usuario
+    Screen.debugPrint(5,5, hola, colores.amarillo, TOP_SCREEN)	
+	Screen.debugPrint(5,15,cumple, colores. blanco, TOP_SCREEN)
+	Screen.debugPrint(5,25, firmware, colores.blanco, TOP_SCREEN)
+	Screen.debugPrint(5,35, kernel, colores.blanco, TOP_SCREEN)
+	Screen.debugPrint(0,0, "Pulsa B para cerrar", colores.blanco, BOTTOM_SCREEN)
+    Screen.flip()
+end
+
+function getVariables()
+	usuario = System.getUsername() 
+	cumpledia, cumplemes = System.getBirthday()
+	firm1, firm2, firm3 = System.getFirmware() 
+	kernel1, kernel2, kernel3 = System.getKernel() 
+	region = System.getRegion() 
+	bateria = System.getBatteryLife() 
+	h,m,s = System.getTime() 
+	d, mes, ano = System.getDate() 
+	if d == 1 then d = "Lun" end
+	if d == 2 then d = "Mar" end
+	if d == 3 then d = "Mie" end
+	if d == 4 then d = "Jue" end
+	if d == 5 then d = "Vie" end
+	if d == 6 then d = "Sab" end
+	if d == 7 then d = "Dom" end
+	
+
+	fecha = d + '/' + mes + '/' + ano
+	hora = h + ':' + m + ':' + s
+	cumple = "Cumple:" + cumpledia + '-' + cumplemes
+	firmware = "Ver. " + firm1 + firm2 + firm3 + ' ' + region
+	kernel = "Kernel: " + kernel1 + kernel2 + kernel3
+	bateria = bateria + "%"
+	
+end
+
+main()
+while true do
+	if Controls.check(pad,KEY_B) and not Controls.check(antpad,KEY_B) then
+		System.exit()	
+	end		
+	Screen.refresh()
+	Screen.debugPrint(0,0, hora, colores.verde, TOP_SCREEN)
+	Screen.debugPrint(15,0, fecha, colores.verde, TOP_SCREEN)
+	Screen.debugPrint(50, 0, bateria, colores.rojo, TOP_SCREEN)
+	main()
+end
